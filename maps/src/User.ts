1670ea0adc951f917483c302b4a76449cf20f751
HoangZoan <d.hoang1906@gmail.com>
2022-04-13 21:35:32 +0700

    Update: Apendix session done

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,11 +1,21 @@
 import faker from "faker";
 
-class User {
+export class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: +faker.address.latitude(),
+      lng: +faker.address.longitude(),
+    };
+  }
+
+  markerContent(): string {
+    return "User Name: " + this.name;
+  }
 }
